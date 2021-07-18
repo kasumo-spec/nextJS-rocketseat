24c0077469df612da94436186e9677326e584fee
Allef Moraes <allefadolpho@hotmail.com>
2021-07-18 17:54:43 -0300

    Save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from './home.module.scss';
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
@@ -36,20 +36,23 @@ const Home = ({ product }:HomeProps) => {
 
 export default Home;
 
-export const getServerSideProps:GetServerSideProps = async () => {
+export const getStaticProps:GetStaticProps = async () => {
   	const price = await stripe.prices.retrieve("price_1JEduUAyKGV1N8G8JBrsp41R")
 
-  	const product = {
+
+  const product = {
   	  priceId: price.id,
 	  amount: new Intl.NumberFormat('en-US', {
 	    style: 'currency',
 		currency: 'USD'
-	  }).format((price.unit_amount / 100)),
+		// @ts-ignore
+	  }).format(price.unit_amount / 100),
 	}
 
   	return {
 		props: {
 		  product
-		}
+		},
+	  	revalidate: 60 * 60 * 24
 	}
 };
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,12 +1,12 @@
 import Stripe from 'stripe'
-import { version } from '../../package.json'
 
 export const stripe = new Stripe(
+	// @ts-ignore
 	process.env.STRIPE_API_KEY, {
 	  apiVersion: '2020-08-27',
 	  appInfo: {
 	    name: 'News Distribution',
-		version
+		version: '0.1.0'
 	  }
 	}
 )
