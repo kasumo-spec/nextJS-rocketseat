d2e4220002527349f2b61442d0098022f65c3580
Allef Moraes <allefadolpho@hotmail.com>
2021-07-26 00:13:22 -0300

    Save

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ const Home = ({ product }:HomeProps) => {
   return (
 	  <>
 		<Head>
-		  <title>Home | ig.news</title>
+		  <title>Home | News Subscription</title>
 		</Head>
 		<main className={ styles.contentContainer }>
 		  <section className={ styles.hero }>
