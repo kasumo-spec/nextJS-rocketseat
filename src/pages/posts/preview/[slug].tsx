c517e8c924a165091016a53570cdade13d5cf2e6
Allef Moraes <allefadolpho@hotmail.com>
2021-07-31 21:49:44 -0300

    Seduzente

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,11 +1,12 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import { getSession } from "next-auth/client";
-import { getPrismicClient } from "../../services/prismic";
+import { getPrismicClient } from "../../../services/prismic";
 import { RichText } from "prismic-dom";
 import Head from "next/head";
-import styles from "./post.module.scss";
+import styles from "../post.module.scss";
+import { Link } from "next/link";
 
-interface PostsProps {
+interface PostsPreviewProps {
   post: {
     slug: string;
     title: string;
@@ -14,7 +15,7 @@ interface PostsProps {
   };
 }
 
-const Post = ({ post }: PostsProps) => {
+const Post = ({ post }: PostsPreviewProps) => {
   return (
     <>
       <Head>
@@ -25,9 +26,15 @@ const Post = ({ post }: PostsProps) => {
           <h1>{post.title}</h1>
           <time>{post.updatedAt}</time>
           <div
-            className={styles.postContent}
+            className={`${styles.postContent} ${styles.previewContent}`}
             dangerouslySetInnerHTML={{ __html: post.content }}
           />
+          <div className={styles.continueReading}>
+            Wanna continue reading?
+            <Link>
+              <a href=""></a>
+            </Link>
+          </div>
         </article>
       </main>
     </>
@@ -36,30 +43,24 @@ const Post = ({ post }: PostsProps) => {
 
 export default Post;
 
-export const getServerSideProps: GetServerSideProps = async ({
-  req,
-  params,
-}) => {
-  const session = await getSession({ req });
+export const getStaticPaths = () => {
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { slug } = params;
 
-  if (!session.activeSubscription) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-      },
-    };
-  }
-
-  const prismic = getPrismicClient(req);
+  const prismic = getPrismicClient();
 
   const response = await prismic.getByUID("publication", String(slug), {});
 
   const post = {
     slug,
     title: RichText.asText(response.data.title),
-    content: RichText.asHtml(response.data.content),
+    content: RichText.asHtml(response.data.content.splice(0, 3)),
     updatedAt: new Date(response.last_publication_date).toLocaleDateString(
       "pt-BR",
       {
